46e584c8cc0ca5d937a00cb4f9bd489c6744e2c4
Justin Combs <skrabio@gmail.com>
2015-04-29 22:19:23 -0400

    landing images and timeout transition

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,28 @@
 'use strict';
 
+
 $(window).scroll(function(){
+
     var wScroll = $(this).scrollTop();
-    $('.logo').css({'transform': 'translate(0px, '+ wScroll /2 +'%)'});
-    $('.onepencil').css({'transform': 'translate(0px, '+ wScroll /4 +'%)'});
-    $('.redpencil').css({'transform': 'translate(0px, '+ wScroll /2.1 +'%)'});
-    $('.pencilcup').css({'transform': 'translate(0px, -'+ wScroll /80 +'%)'});
+
+    $('.logo').css({'transform': 'translate(0px, '+ wScroll /2.4 +'%)'});
+    $('.onepencil').css({'transform': 'translate(0px, '+ wScroll /8 +'%)'});
+    $('.redpencil').css({'transform': 'translate(0px, '+ wScroll /8 +'%)'});
+    // $('.pencilcup').css({'transform': 'translate(0px, -'+ wScroll /80 +'%)'});
+
+    if(wScroll > $('.sketches').offset().top -
+            ($(window).height() / 1.2)) {
+
+        $('.sketches figure').each(function(i){
+
+            setTimeout(function(){
+
+            $('.sketches figure').eq(i).addClass('is-showing');
+
+            }, 150 * (i + 1));
+
+        });
+
+    }
+
 });
